4f1450befbb8692fd3f5a9d844c1dc533d5747cf
aleknots <60971316+aleknots@users.noreply.github.com>
2021-03-01 19:47:23 -0300

    commit

--- db/script.sql
+++ db/script.sql
@@ -1,12 +1,13 @@
-CREATE DATABASE IF NOT EXISTS tabelacursos;
-USE tabelacursos;
+CREATE DATABASE IF NOT EXISTS vendas;
+USE vendas;
 
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS cursos (
   id INT(11) AUTO_INCREMENT,
   name VARCHAR(255),
   price DECIMAL(10, 2),
   PRIMARY KEY (id)
 );
 
-INSERT INTO products VALUE(0, 'Curso Front-end especialista', 2500);
-INSERT INTO products VALUE(0, 'Curso JS Fullstack', 900);
+INSERT INTO cursos VALUE(0, 'Curso Front-end especialista', 2500);
+INSERT INTO cursos VALUE(0, 'Curso JS Fullstack', 900);
+INSERT INTO cursos VALUE(0, 'Curso JS Fullstack', 900);
